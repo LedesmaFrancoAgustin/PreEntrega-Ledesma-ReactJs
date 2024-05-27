46a836ddbe0a12f88bbbf0483b062603db90fcc5
LedesmaFrancoAgustin <ledesmafranco50@gmail.com>
2024-05-27 11:25:33 -0300

    Correcion de Errores

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,23 @@ import BuyProduct from './components/paginas/BuyProduct';
 import PageNotFound from "./components/paginas/PageNotFound"
 
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYkeCt4mLXXnzA5u02rUI8mSiS7s8BFaw",
+  authDomain: "coderhouse-ecommerce-291cc.firebaseapp.com",
+  projectId: "coderhouse-ecommerce-291cc",
+  storageBucket: "coderhouse-ecommerce-291cc.appspot.com",
+  messagingSenderId: "232101529614",
+  appId: "1:232101529614:web:4145292d8ceb852a90145f"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 
 
